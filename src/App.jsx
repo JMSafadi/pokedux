b8fa1078fcdb124b2e66318b302c715746c8ac4d
Julian Safadi <julianmatiassafadi@hotmail.com>
2023-02-06 15:15:12 -0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <div className="App">
-      <Col span={4} offset={10} >
+      <Col span={5} offset={10} >
         <img src={logo} alt='logo'/>
       </Col>
       <Col span={8} offset={8}>
